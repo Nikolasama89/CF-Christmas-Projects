fe3921a84687f5013a937fe863689ec2feda4c74
Nikos <niqos89@gmail.com>
2025-01-03 14:15:52 +0200

    added playersSymbolPick method

--- src/gr/aueb/cf/project4/TrilizaGame.java
+++ src/gr/aueb/cf/project4/TrilizaGame.java
@@ -1,11 +1,15 @@
 package gr.aueb.cf.project4;
 
+import java.util.Scanner;
+
 public class TrilizaGame {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         char[][] board = new char[3][3];
         startingBoard(board);
         printBoard(board);
+        char player1 = playersSymbolPick(scanner);
     }
 
     public static void startingBoard(char[][] board) {
@@ -23,6 +27,23 @@ public class TrilizaGame {
             }
             System.out.println();
         }
-
     }
+
+    public static char playersSymbolPick(Scanner scanner) {
+        char symbol;
+
+        while (true) {
+            System.out.println("Please pick X or O: ");
+            symbol = scanner.next().toUpperCase().charAt(0);
+
+            if (symbol == 'X' || symbol == 'O') {
+                break;
+            } else {
+                System.out.println("Please pick a valid option!");
+            }
+        }
+        System.out.println("Player 1 picks: " + symbol);
+        return symbol;
+    }
+
 }
